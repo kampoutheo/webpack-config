9ff0983917137d29152d75b88f8257f07a4195ae
Theocharis Kampouridis <kampou.theo@yahoo.gr>
2023-02-21 22:32:11 +0200

    Add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ const elvenShieldRecipe = {
    refinedMoonstone: 4,
 };
 
+const elvenGauntlesRecipe = {
+   ... elvenShieldRecipe,
+   leather: 1,
+   refinedMoonstone:4,
+}
+
 console.log(elvenShieldRecipe);
+
+console.log(elvenGauntlesRecipe);
